fab6591b06c5f71c8ec3b65ee91131f6f9cd4e36
Rijal Rifai <rifairijal97@gmail.com>
2026-05-15 16:12:33 +0700

    update middleware trust proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Cukup tambahkan baris ini di dalam blok bawaannya
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
